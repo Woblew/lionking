6c301fe90b0bbb452277e873dfe9ca6be348ec0a
Woblew <68404082+Woblew@users.noreply.github.com>
2021-08-16 23:49:32 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,34 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-nats-ingonya-ba-bagithi-baba-sithi-uhm-ingonya-aba",
+      "name": "Nats ingonya ba bagithi Baba Sithi uhm ingonya aba",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": -0.3317758597966929,
+        "pitch": 0.21981478718034708,
+        "fov": 1.49556146710482
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Lion King",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
